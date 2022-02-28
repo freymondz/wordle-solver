4e9865474b6a6449ac0eeed1d1c15c54091634de
ZacZhang <zachaz@uw.edu>
2022-02-28 15:37:24 -0800

    initial commit

---
+++ wordProcessor.py
@@ -0,0 +1,15 @@
+import json
+
+def load_words():
+    with open('words_dictionary.json') as json_file:
+        data = json.load(json_file)
+    words = data.keys()
+    words_json = {}
+    for word in words:
+        if len(word) == 5:
+            words_json[word] = 1
+    with open('wordFile', 'w') as wordFile:
+        json.dump(words_json, wordFile)
+
+if __name__ == '__main__':
+    english_words = load_words()
